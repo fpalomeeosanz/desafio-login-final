b0fdd5f2eaa5a2ba640a0cf2ed5d59c757b537ac
fpalomerosanz <fpalomerosanz@gmail.com>
2024-03-05 06:50:14 -0300

    handlebars

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,5 @@
 export function auth(req, res, next) {
-    if(req.session?.user === 'adminCoder@coder.com' + req.session?.password === 'adminCod3r123' && req.session?.admin){
+    if(req.session?.user === 'adminCoder@coder.com' + req.session?.password === 'adminCod3r123'){
         return next();
     }
     return res.status(401).send('error de autorizacion')
